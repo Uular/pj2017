9a60ac3640dabaf035bbbb91a07a66ddafcc1786
niriikon <niriikon@gmail.com>
2016-11-29 18:56:01 +0200

    Mr. Pokémon champion

--- index2.php
+++ index2.php
@@ -133,6 +133,16 @@
                         <img src="img/portfolio/pelimanne.png" class="img-responsive" alt="">
                     </a>
                 </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/poke.png" class="img-responsive" alt="">
+                    </a>
+                </div>
             </div>
         </div>
     </section>
@@ -311,6 +321,29 @@
                             <hr class="star-primary">
                             <img src="img/portfolio/pelimanne.png" class="img-responsive img-centered" alt="">
                             <p>Eeppinen seremoniamestari ja lottomusiikin mestari.</p>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Hallitseva pokémon-mestari</h2>
+                            <hr class="star-primary">
+                            <img src="img/poke.png" class="img-responsive img-centered" alt="">
+                            <p>Mutta entäs vyö, missä se on?</p>
                             <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
                         </div>
                     </div>
